51f75f93efcf6b011d28ad52ddf01a37a6d66cdb
Gohar Ghukasyan <gohar.ghukasyan2506@gmail.com>
2022-07-22 11:08:20 +0400

    1-4 homework (medium)

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) < 1:
+            return s
+
+        def isPalindrome(left, right):
+            return s[left:right] == s[left:right][::-1]
+
+        left, right = 0, 1
+        for index in range(1, len(s)):
+            if index - right > 0 and isPalindrome(index - right - 1, index + 1):
+                left, right = index - right - 1, right + 2
+            if index - right >= 0 and isPalindrome(index - right, index + 1):
+                left, right = index - right, right + 1
+        return s[left: left + right]
